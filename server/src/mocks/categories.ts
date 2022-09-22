7d9ce0a841d157b34b0cfa49221c9e5aca2b716c
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-09-22 20:13:44 -0300

    mock endpoints to transactions and categories

---
+++ server/src/mocks/categories.ts
@@ -0,0 +1,24 @@
+const categories = [
+  {
+    id: 1,
+    name: 'Equipamentos de TI, assinatura de softwares e cursos',
+  },
+  {
+    id: 2,
+    name: 'Salário e freelas',
+  },
+  {
+    id: 3,
+    name: 'Impostos',
+  },
+  {
+    id: 4,
+    name: 'Entretenimento',
+  },
+  {
+    id: 5,
+    name: 'Investimentos',
+  },
+];
+
+module.exports = categories;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 
+const transactions = require('./mocks/transactions');
+const categories = require('./mocks/categories');
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  return res.json('hello world');
+app.get('/transactions', (req, res) => {
+  return res.json(transactions);
+});
+
+app.get('/categories', (req, res) => {
+  return res.json(categories);
 });
 
 app.listen('3333');
