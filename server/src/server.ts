8083a7d608795ea9cba5584a5957ab4da261eff6
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-09-22 22:22:18 -0300

    create endpoint to get transactions and categories

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,37 @@
 import express from 'express';
 import cors from 'cors';
-
-const transactions = require('./mocks/transactions');
-const categories = require('./mocks/categories');
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/transactions', (req, res) => {
+const prisma = new PrismaClient({
+  log: ['query'],
+});
+
+app.get('/transactions', async (req, res) => {
+  const transactions = await prisma.transactions.findMany({
+    select: {
+      id: true,
+      description: true,
+      date: true,
+      categoriesId: true,
+    },
+  });
+
   return res.json(transactions);
 });
 
-app.get('/categories', (req, res) => {
+app.get('/categories', async (req, res) => {
+  const categories = await prisma.categories.findMany({
+    select: {
+      id: true,
+      name: true,
+      _count: true,
+    },
+  });
+
   return res.json(categories);
 });
 
